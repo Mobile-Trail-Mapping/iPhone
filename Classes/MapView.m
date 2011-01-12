fe57009892c6ac1446ba506c811f578b2a148bae
Pete Brousalis <brousapg@gmail.com>
2011-01-12 14:02:13 -0500

    More fixes on XML parsing. Close

--- Classes/MapView.m
+++ Classes/MapView.m
@@ -33,23 +33,32 @@
     
     CXMLDocument *doc = [[[CXMLDocument alloc] initWithContentsOfURL:url options:0 error:nil] autorelease];
     
-    NSArray *nodes = [doc nodesForXPath:@"//trail/points/point" error:nil];
-    
-    for (CXMLElement *node in nodes) {
-    
-        NSMutableDictionary *item = [[NSMutableDictionary alloc] init];
-        
-        int counter;
-        for(counter = 0; counter < [node childCount]; counter++) {
-        
-            [item setObject:[[node childAtIndex:counter] stringValue] forKey:[[node childAtIndex:counter] name]];
+    NSMutableDictionary *item = [[NSMutableDictionary alloc] init];
+
+    for (CXMLElement *trails in data) {
+
+        NSArray *trails = [doc nodesForXPath:@"//trails/trail" error:nil];
+
+        for (CXMLElement *trail in trails) {
+            [item setObject:[[trail attributeForName:@"id"] stringValue] forKey:@"trail_id"];
         }
         
-        [item setObject:[[node attributeForName:@"id"] stringValue] forKey:@"id"];
-        [_xmlPoints addObject:[item copy]];
+        NSArray *nodes = [doc nodesForXPath:@"//trail/points/point" error:nil];
         
-        [item release];
+        for (CXMLElement *node in nodes) {
+
+            int counter;
+            for(counter = 0; counter < [node childCount]; counter++) {
+            
+                [item setObject:[[node childAtIndex:counter] stringValue] forKey:[[node childAtIndex:counter] name]];
+            }
+            [item setObject:[[node attributeForName:@"id"] stringValue] forKey:@"point_id"];
+        }
+
     }
+    
+    [_xmlPoints addObject:[item copy]];
+    [item release];
 }
 
 #pragma mark -
