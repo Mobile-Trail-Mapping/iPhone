c94cc14ccd3c609c4c8ccee995c4d345bcd1531a
Tim Ekl <lithium3141@gmail.com>
2011-01-21 14:30:37 -0500

    Start documentation effort; import Doxyfile, update .gitignore

--- Classes/iPhoneViewController.h
+++ Classes/iPhoneViewController.h
@@ -2,6 +2,11 @@
 #import "MapView.h"
 #import "TrailPoint.h"
 
+/**
+ * The primary view controller for the iPhone MTM application. Contains an
+ * instance of MKMapView for trails display, and is the first view controller
+ * instantiated after application launch.
+ */
 @interface iPhoneViewController : UIViewController {
     MKMapView * _mapView;
 }
