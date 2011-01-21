613e87c2e0db9f7a310eae4aee78d2fe82fc6752
Tim Ekl <lithium3141@gmail.com>
2011-01-21 16:08:19 -0500

    Continue documentation

--- Classes/InterestPoint.h
+++ Classes/InterestPoint.h
@@ -3,10 +3,15 @@
 #import <CoreLocation/CoreLocation.h>
 #import <MapKit/MapKit.h>
 
-// TODO are there issues in converting back and forth from CGPoint to CLLocationCoordinate2D?
-
+/**
+ * Primary class representing any marked point on a trail. InterestPoint
+ * objects range from describing simple locations through which a trail
+ * passes (defining the path of the trail) to representing full-blown
+ * locations of historical interest.
+ */
 @interface InterestPoint : NSObject {
 
+@private
     NSInteger _pointID;
     CLLocationCoordinate2D _location;
 	NSString* _category;
@@ -15,13 +20,39 @@
     NSInteger _categoryID;
 }
 
+/**
+ * The unique ID of this point.
+ */
 @property (nonatomic) NSInteger pointID;
+
+/**
+ * The coordinate (latitude/longitude pair) of this point.
+ */
 @property (nonatomic) CLLocationCoordinate2D location;
+
+/**
+ * The category to which this point belongs.
+ */
 @property (nonatomic, retain) NSString* category;
+
+/**
+ * The title of this point. Optional for points of category "Trail".
+ */
 @property (nonatomic, retain) NSString* title;
+
+/**
+ * The color to paint this point. Optional.
+ */
 @property (nonatomic, retain) UIColor* color;
+
+/**
+ * The unique ID of this point's category.
+ */
 @property (nonatomic) NSInteger categoryID;
 
+/**
+ * Create a new point with the given parameters. Designated initializer.
+ */
 -(id) initWithID:(NSInteger)pointID location:(CLLocationCoordinate2D)p category:(NSString *)c title:(NSString *)t;
 
 @end
--- Classes/TrailPoint.h
+++ Classes/TrailPoint.h
@@ -2,20 +2,70 @@
 #import "InterestPoint.h"
 #import "Trail.h"
 
+/**
+ * A type of InterestPoint which belongs to a Trail object. TrailPoint objects
+ * are commonly used to represent path points of a Trail or trail heads.
+ */
 @interface TrailPoint : InterestPoint {
-    
+@private
     Trail *_trail;
     NSMutableSet *_connections;
     NSMutableArray *_unresolvableLinks;
     BOOL _hasUnresolvedLinks;
 }
 
+/**
+ * The Trail to which these TrailPoint objects belong.
+ */
 @property (nonatomic, retain) Trail *trail;
-@property (nonatomic, retain) NSMutableSet *connections;
-@property (nonatomic, retain) NSMutableArray *unresolvableLinks;
-@property (nonatomic, assign) BOOL hasUnresolvedLinks;
 
+/**
+ * The set of TrailPoint objects to which this TrailPoint is connected.
+ * TrailPoint connections are not symmetric; it is not necessarily true that
+ * this TrailPoint is in the connections set of its connected points.
+ */
+@property (nonatomic, retain) NSMutableSet *connections;
+
+/**
+ * The set of trail point IDs that could not be resolved into true
+ * TrailPoint objects. Primarily used during initial parsing to temporarily
+ * store IDs of linked points within the same trail until those points
+ * can be parsed and resolved.
+ *
+ * @see TrailPoint#hasUnresolvedLinks
+ * @see TrailPoint#resolveLinksWithinTrail:
+ */
+@property (nonatomic, retain) NSMutableArray *unresolvableLinks;
+
+/**
+ * Whether this TrailPoint has unresolved links that need conversion to
+ * TrailPoint objects. Effectively the same as calling:
+ *
+ * <pre><code>[[trailPoint unresolvableLinks] count] &gt; 0</code></pre>
+ *
+ * @see TrailPoint#unresolvableLinks
+ * @see TrailPoint#resolveLinksWithinTrail:
+ */
+@property (nonatomic, readonly) BOOL hasUnresolvedLinks;
+
+/**
+ * Create a new TrailPoint with the given information and connections set.
+ * Primarily called by DataParser to assign TrailPoint objects to Trail
+ * objects, but may be called separately for custom TrailPoint creation.
+ *
+ * If the <code>connections</code> parameter is <code>nil</code>, creates
+ * an empty connections set which can be manipulated later.
+ */
 - (id)initWithID:(NSInteger)pointID location:(CLLocationCoordinate2D)p category:(NSString *)c title:(NSString *)t connections:(NSMutableSet *)connections;
+
+/**
+ * Iterate through the set of unresolved connections on this TrailPoint,
+ * evaluating each to a TrailPoint belonging to the given Trail object. Removes
+ * resolved point IDs from TrailPoint#unresolvableLinks on resolution.
+ *
+ * @see TrailPoint#unresolvableLinks
+ * @see TrailPoint#resolveLinksWithinTrail:
+ */
 - (void)resolveLinksWithinTrail:(Trail *)trail;
 
 @end
