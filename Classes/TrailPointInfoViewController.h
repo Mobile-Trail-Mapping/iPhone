2b2b5e3f0f03b024e55188996f9db7bb79e38fae
Tim Ekl <lithium3141@gmail.com>
2011-02-07 01:13:35 -0500

    Implement image loading and rotation for trail head detail views

--- Classes/TrailPointInfoViewController.h
+++ Classes/TrailPointInfoViewController.h
@@ -4,10 +4,16 @@
 
 @class TrailPoint;
 
-@interface TrailPointInfoViewController : UIViewController {
+@interface TrailPointInfoViewController : UIViewController <UITableViewDelegate, UITableViewDataSource> {
 @private
     id<TrailPointInfoModalDelegate> _delegate;
     TrailPoint * _trailPoint;
+    
+    UIImageView * _imageView;
+    UITableView * _tableView;
+    UIActivityIndicatorView * _activityIndicatorView;
+    
+    NSMutableArray * _images;
 }
 
 /**
@@ -24,9 +30,53 @@
 @property (nonatomic, retain) TrailPoint * trailPoint;
 
 /**
+ * The on-screen view which displays images associated with this controller's
+ * TrailPoint.
+ * @see TrailPointInfoViewController#trailPoint
+ */
+@property (nonatomic, retain) IBOutlet UIImageView * imageView;
+
+/**
+ * The on-screen view which displays detailed information about this
+ * controller's TrailPoint.
+ * @see TrailPointInfoViewController#trailPoint
+ */
+@property (nonatomic, retain) IBOutlet UITableView * tableView;
+
+/**
+ * A basic activity indicator to show image load progress.
+ * @see TrailPointInfoViewController#imageView
+ */
+@property (nonatomic, retain) IBOutlet UIActivityIndicatorView * activityIndicatorView;
+
+/**
+ * The set of images associated with this controller's TrailPoint. Lazy-loaded
+ * on each access to the controller. Caching is not implemented at this time.
+ */
+@property (nonatomic, retain) NSMutableArray * images;
+
+/**
  * Dismiss the modal controller being displayed, if any. Primarily used to
  * hide instances of TrailPointInfoViewController.
  */
 - (IBAction)dismiss:(id)sender;
+
+/**
+ * Load remote images for this controller's TrailPoint. This method is
+ * intended for use as the launching point of a background thread; it
+ * establishes an autorelease pool and dispatches calls to the main thread
+ * for UI operations as necessary. Callers should be aware of the conventions
+ * used by this method and subsequent load... methods.
+ */
+- (void)loadRemoteImage;
+
+- (void)showFailureImage;
+
+/**
+ * Notification method that informs this controller a new image has been
+ * downloaded from the MTM server and is ready for display in association
+ * with this controller's TrailPoint.
+ */
+- (void)registerImage:(UIImage *)image;
 
 @end
--- Classes/TrailPointInfoViewController.m
+++ Classes/TrailPointInfoViewController.m
@@ -6,6 +6,11 @@
 @synthesize delegate = _delegate;
 @synthesize trailPoint = _trailPoint;
 
+@synthesize imageView = _imageView;
+@synthesize tableView = _tableView;
+@synthesize activityIndicatorView = _activityIndicatorView;
+@synthesize images = _images;
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     if((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil])) {
         [self addObserver:self forKeyPath:@"trailPoint" options:0 context:nil];
@@ -13,8 +18,22 @@
     return self;
 }
 
+- (void)viewDidAppear:(BOOL)animated {
+    if(self.imageView.image == nil) {
+        [self performSelectorInBackground:@selector(loadRemoteImage) withObject:nil];
+    }
+}
+
 - (void)dealloc {
     [self removeObserver:self forKeyPath:@"trailPoint"];
+    
+    [_trailPoint release];
+    [_imageView release];
+    [_tableView release];
+    [_activityIndicatorView release];
+    [_images release];
+    
+    NSLog(@"dealloc %@", self);
     
     [super dealloc];
 }
@@ -35,6 +54,93 @@
 
 - (IBAction)dismiss:(id)sender {
     [self.delegate dismissModalController];
+}
+
+#pragma mark -
+#pragma mark Async image-loading methods
+
+- (void)showFailureImage {
+    [self.imageView setImage:[UIImage imageNamed:@"redx.png"]];
+}
+
+- (void)loadRemoteImage {
+    NSLog(@"loading remote images");
+    NSLog(@"  for point with ID %d", self.trailPoint.pointID);
+    NSInteger trailPointID = self.trailPoint.pointID;
+    
+    NSAutoreleasePool * threadPool = [[NSAutoreleasePool alloc] init];
+    
+    [self.activityIndicatorView performSelectorOnMainThread:@selector(startAnimating) withObject:nil waitUntilDone:YES];
+    
+    NSString * imageCountURLString = [[[NSString alloc] initWithFormat:@"http://mtmserver.heroku.com/image/get/%d", trailPointID] autorelease];
+    NSURL * imageCountURL = [[[NSURL alloc] initWithString:imageCountURLString] autorelease];
+    NSStringEncoding usedEncoding;
+    NSError * error;
+    NSString * imageCountString = [[[NSString alloc] initWithContentsOfURL:imageCountURL usedEncoding:&usedEncoding error:&error] autorelease];
+    
+    if(imageCountString == nil) {
+        [self showFailureImage];
+    } else {
+        NSScanner * imageCountScanner = [[[NSScanner alloc] initWithString:imageCountString] autorelease];
+        NSInteger imageCount = 0;
+        if(![imageCountScanner scanInteger:&imageCount] || imageCount == 0) {
+            [self showFailureImage];
+        } else {
+            NSLog(@"Found %d images for trail point with ID %d", imageCount, self.trailPoint.pointID);
+            if(self.images == nil) {
+                self.images = [[[NSMutableArray alloc] initWithCapacity:imageCount] autorelease];
+            }
+            
+            for(int i = 0; i < imageCount; i++) {
+                NSString * imageURLString = [[[NSString alloc] initWithFormat:@"http://mtmserver.heroku.com/image/get/%d/%d", trailPointID, i] autorelease];
+                NSURL * imageURL = [[[NSURL alloc] initWithString:imageURLString] autorelease];
+                NSData * data = [[[NSData alloc] initWithContentsOfURL:imageURL] autorelease];
+                if(data != nil) {
+                    UIImage * image = [[[UIImage alloc] initWithData:data] autorelease];
+                    [self registerImage:image];
+                }
+            }
+        }
+    }
+    
+    [self.activityIndicatorView performSelectorOnMainThread:@selector(stopAnimating) withObject:nil waitUntilDone:YES];
+    
+    self.imageView.animationImages = self.images;
+    self.imageView.animationDuration = (NSTimeInterval) self.images.count * 3.0;
+    [self.imageView performSelectorOnMainThread:@selector(startAnimating) withObject:nil waitUntilDone:YES];
+    
+    [threadPool drain];
+}
+
+- (void)registerImage:(UIImage *)image {
+    if(self.images == nil) {
+        self.images = [[[NSMutableArray alloc] initWithCapacity:10] autorelease];
+    }
+    [self.images addObject:image];
+}
+
+#pragma mark -
+#pragma mark UITableViewDataSource methods
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString * kCellReuseIdentifier = @"Cell";
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:kCellReuseIdentifier];
+    if(cell == nil) {
+        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:kCellReuseIdentifier] autorelease];
+    }
+    
+    // Configure cell
+    cell.textLabel.text = @"Hello world";
+    
+    return cell;
 }
 
 @end
