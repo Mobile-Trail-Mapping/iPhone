8dadfd5b6f254cd06f18263659163f1f44233c51
Tim Ekl <lithium3141@gmail.com>
2011-03-29 21:02:55 -0400

    Stub out view controller for adding problem reports

---
+++ Classes/AddProblemReportViewController.h
@@ -0,0 +1,16 @@
+//
+//  AddProblemReportViewController.h
+//  MTM
+//
+//  Created by Tim Ekl on 3/29/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface AddProblemReportViewController : MTMSettingsViewController {
+    
+}
+
+@end
